046a1ca12ccf88be326fadd3a1f456a217c84e64
felixrydberg <felixryd@gmail.com>
2022-10-10 18:15:50 +0200

    boilerplate

---
+++ boilerPlate/src/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import logo from './logo.svg';
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <h1>Hello gamers</h1>
+    )
+  }
+}
